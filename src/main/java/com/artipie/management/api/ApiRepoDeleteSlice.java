761ad8d11169694d5bacebfac4a7bb9f46116e97
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2021-11-24 12:58:10 +0300

    feat: added slice for repo delete

---
+++ src/main/java/com/artipie/management/api/ApiRepoDeleteSlice.java
@@ -0,0 +1,160 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/management-api/LICENSE.txt
+ */
+package com.artipie.management.api;
+
+import com.artipie.ArtipieException;
+import com.artipie.asto.FailedCompletionStage;
+import com.artipie.asto.Key;
+import com.artipie.asto.Storage;
+import com.artipie.asto.ext.PublisherAs;
+import com.artipie.http.Headers;
+import com.artipie.http.Response;
+import com.artipie.http.Slice;
+import com.artipie.http.async.AsyncResponse;
+import com.artipie.http.rq.RequestLineFrom;
+import com.artipie.http.rs.RsStatus;
+import com.artipie.http.rs.RsWithBody;
+import com.artipie.http.rs.RsWithHeaders;
+import com.artipie.http.rs.RsWithStatus;
+import com.artipie.management.ConfigFiles;
+import java.net.URLDecoder;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.reactivestreams.Publisher;
+
+/**
+ * Repo {@code DELETE} API.
+ * @since 0.5
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ * @todo #321:30min Remove code duplication.
+ *  In this class and in the class with tests for this one
+ *  a pair of very similar utils methods are used which
+ *  are also used in classes for update repo. It is necessary
+ *  to eliminate this code duplication.
+ */
+public final class ApiRepoDeleteSlice implements Slice {
+    /**
+     * URI path pattern.
+     */
+    private static final Pattern PTN = Pattern.compile(
+        "/api/repos/(?<user>[^/?.]+)\\?method=delete$"
+    );
+
+    /**
+     * Config file to support `yaml` and `.yml` extensions.
+     */
+    private final ConfigFiles configfile;
+
+    /**
+     * Storage.
+     */
+    private final Storage storage;
+
+    /**
+     * Ctor.
+     * @param storage Storage
+     * @param configfile Config file to support `yaml` and `.yml` extensions
+     */
+    public ApiRepoDeleteSlice(final Storage storage, final ConfigFiles configfile) {
+        this.storage = storage;
+        this.configfile = configfile;
+    }
+
+    @Override
+    public Response response(
+        final String line,
+        final Iterable<Map.Entry<String, String>> headers,
+        final Publisher<ByteBuffer> body
+    ) {
+        final Matcher matcher = PTN.matcher(new RequestLineFrom(line).uri().getPath());
+        if (!matcher.matches()) {
+            throw new ArtipieException(
+                String.format(
+                    "Uri '%s' does not match to the pattern '%s'", line, ApiRepoDeleteSlice.PTN
+                )
+            );
+        }
+        final String user = matcher.group("user");
+        return new AsyncResponse(
+            new PublisherAs(body).asciiString()
+                .thenApply(form -> URLDecoder.decode(form, StandardCharsets.US_ASCII)).thenCompose(
+                    form -> {
+                        final String name = ApiRepoDeleteSlice.value(form, "repo");
+                        final Key repo = new Key.From(user, String.format("%s.yaml", name));
+                        return this.configfile.exists(repo)
+                            .thenCompose(
+                                exists -> {
+                                    final CompletionStage<Void> res;
+                                    if (exists) {
+                                        res = this.configfile.delete(repo)
+                                            .thenCompose(
+                                                noth -> this.storage.list(
+                                                    new Key.From(user, name)
+                                                ).thenCompose(
+                                                    keys -> CompletableFuture.allOf(
+                                                        keys.stream().map(
+                                                            this.storage::delete
+                                                        ).toArray(CompletableFuture[]::new)
+                                                    )
+                                                )
+                                            );
+                                    } else {
+                                        res = new FailedCompletionStage<>(
+                                            new ArtipieException(
+                                                String.format(
+                                                    "Repo '%s' does not exist", repo.string()
+                                                )
+                                            )
+                                        );
+                                    }
+                                    return res;
+                                }
+                            ).thenApply(nothing -> repo);
+                    }).handle(
+                        (repo, thr) -> {
+                            final Response res;
+                            if (thr == null) {
+                                res = new RsWithHeaders(
+                                    new RsWithStatus(RsStatus.OK),
+                                    new Headers.From(
+                                        "Location", String.format("/dashboard/%s", user)
+                                    )
+                                );
+                            } else if (thr.getCause() instanceof ArtipieException) {
+                                res = new RsWithBody(
+                                    new RsWithStatus(RsStatus.BAD_REQUEST),
+                                    String.format("Failed to delete repo '%s'", repo),
+                                    StandardCharsets.UTF_8
+                                );
+                            } else {
+                                res = new RsWithStatus(RsStatus.INTERNAL_ERROR);
+                            }
+                            return res;
+                        }
+                )
+        );
+    }
+
+    /**
+     * Obtain value from payload, payload is a query string (not url-encoded):
+     * <code>name1=value1&name2=value2</code>.
+     * @param payload Payload to parse
+     * @param name Parameter name to obtain
+     * @return Parameter value
+     */
+    private static String value(final String payload, final String name) {
+        final int start = payload.indexOf(String.format("%s=", name)) + name.length() + 1;
+        int end = payload.indexOf('&', start);
+        if (end == -1) {
+            end = payload.length();
+        }
+        return payload.substring(start, end);
+    }
+}
